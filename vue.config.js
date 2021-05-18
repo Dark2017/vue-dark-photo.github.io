f20090c880c0a2eb79381f8571a6eb60fd831a66
dark <502342826@qq.com>
2021-05-18 17:56:42 +0800

    demo

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  productionSourceMap: false,
+  lintOnSave: false,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html'
+    }
+  }
+}
